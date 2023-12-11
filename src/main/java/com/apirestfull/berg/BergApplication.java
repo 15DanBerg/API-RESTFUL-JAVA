8d8a8b22fc085066da8f8072abeeaf6eac72ec23
15DanBerg <daniel.berg031@gmail.com>
2023-12-11 19:54:18 -0300

    First Commit

---
+++ src/main/java/com/apirestfull/berg/BergApplication.java
@@ -0,0 +1,11 @@
+package com.apirestfull.berg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BergApplication {
+
+	public static void main(String[] args) {
+
+}
---
+++ src/test/java/com/apirestfull/berg/BergApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apirestfull.berg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BergApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
