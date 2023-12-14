515d0f5bf322cb263f6180dafca16ab342865f6c
15DanBerg <daniel.berg031@gmail.com>
2023-12-14 20:39:36 -0300

    Model do Produto criado

---
+++ src/main/java/com/apirestfull/berg/model/Produto.java
@@ -0,0 +1,61 @@
+package com.apirestfull.berg.model;
+
+public class Produto {
+
+    //#region Atributos
+    private int id;
+
+    private String nome;
+    
+    private int quantidade;
+
+    private double valor;
+    
+    private String observacao;
+
+    //#endregion
+
+    //#region Getters e Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public String getObservacao() {
+        return observacao;
+    }
+
+    public void setObservacao(String observacao) {
+        this.observacao = observacao;
+    }
+    
+    //#endregion
+    
+}
