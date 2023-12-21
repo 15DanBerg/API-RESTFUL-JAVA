5e810b93fdbf229d2d6b665d29cbbfc878eef584
15DanBerg <daniel.berg031@gmail.com>
2023-12-21 20:26:28 -0300

    Método delete adicioanado na controller

--- src/main/java/com/apirestfull/berg/controller/ProdutoController.java
+++ src/main/java/com/apirestfull/berg/controller/ProdutoController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -35,5 +36,11 @@ public class ProdutoController {
     public Produto adicionar(@RequestBody Produto produto){
         return produtoService.adicionar(produto);
     }
-}
 
+    @DeleteMapping("/{id}")
+    public String deletar(@PathVariable int id){
+        produtoService.deletar(id);
+        return "Produto com id: " + id + " foi deletado!";
+    }
+}
+ 
