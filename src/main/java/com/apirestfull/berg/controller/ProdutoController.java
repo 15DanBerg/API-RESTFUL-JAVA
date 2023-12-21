e56c32ad567ec59374fe1e6f346bcc54d4c6f740
15DanBerg <daniel.berg031@gmail.com>
2023-12-21 19:41:12 -0300

    Adicionado Post na controller do produto

--- src/main/java/com/apirestfull/berg/controller/ProdutoController.java
+++ src/main/java/com/apirestfull/berg/controller/ProdutoController.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,5 +23,9 @@ public class ProdutoController {
     public List<Produto> obterTodos(){
         return produtoService.obterTodos();
     }
+
+    @PostMapping
+    public Produto adicionar(@RequestBody Produto produto){
+        return produtoService.adicionar(produto);
+    }
 }
- 
