f544d183296b6c14e3a3de5eedccb8567ea8aba9
15DanBerg <daniel.berg031@gmail.com>
2023-12-26 21:16:13 -0300

    Criado o handle para exceptions, adiocioando a exception para notfound

---
+++ src/main/java/com/apirestfull/berg/handle/RestExceptionHandle.java
@@ -0,0 +1,23 @@
+package com.apirestfull.berg.handle;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.apirestfull.berg.model.err.ErrorMensage;
+import com.apirestfull.berg.model.exception.ResourceNotFoundException;
+
+@ControllerAdvice
+public class RestExceptionHandle {
+    
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<?> handleResorceNotFoundExcepition(ResourceNotFoundException ex){
+
+        ErrorMensage error = new ErrorMensage("Not Found", HttpStatus.NOT_FOUND.value() , ex.getMensagem());
+        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+
+    } 
+
+
+}
